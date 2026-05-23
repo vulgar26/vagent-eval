5835f198bf55741b3b1972dad2dad5f01c57e91f
vulgar26 <zly-26@qq.com>
2026-05-23 16:14:16 +0800

    test: move status controller helper tests into matching package

--- src/test/java/com/vagent/eval/VagentEvalApplicationTests.java
+++ src/test/java/com/vagent/eval/VagentEvalApplicationTests.java
@@ -77,24 +77,6 @@ class VagentEvalApplicationTests {
     }
 
     @Test
-    void maskJdbcUrlStripsUserinfoPassword() {
-        assertThat(InternalEvalStatusController.maskJdbcUrl("jdbc:postgresql://u:secret@localhost:5432/eval"))
-                .isEqualTo("jdbc:postgresql://u:***@localhost:5432/eval");
-    }
-
-    @Test
-    void maskJdbcUrlLeavesUrlWithoutEmbeddedPassword() {
-        assertThat(InternalEvalStatusController.maskJdbcUrl("jdbc:postgresql://localhost:5432/eval"))
-                .isEqualTo("jdbc:postgresql://localhost:5432/eval");
-    }
-
-    @Test
-    void maskJdbcUrlRedactsPasswordQueryParam() {
-        assertThat(InternalEvalStatusController.maskJdbcUrl("jdbc:postgresql://localhost/eval?password=secret&ssl=true"))
-                .isEqualTo("jdbc:postgresql://localhost/eval?password=***&ssl=true");
-    }
-
-    @Test
     void datasetCreateImportJsonlAndQuery() throws Exception {
         String createJson = """
                 {"name":"smoke","version":"v1","description":"day2"}
